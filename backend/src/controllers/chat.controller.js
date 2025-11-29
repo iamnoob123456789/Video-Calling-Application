e5bf9338caf20b533bc358ef7ad6bbfb2ff8b9db
iamnoob123456789 <shreyasakula97@gmail.com>
2025-11-29 09:12:47 +0530

    resolved errors in authcontroller.js ,chat.controller.js and user.controller.js

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -1,11 +1,12 @@
-import {generateStreamToken} from "../lib/stream.js";
-export async function getStreamToken(req,res){
-    try{
-       const token=generateStreamToken(req.user.id);
-        res.status(200).json({success:true,token});
+import { generateStreamToken } from "../lib/stream.js";
 
-    }catch(error){
-        console.log(error);
-        res.status(500).json({message:"Internal ServerError"});
-    }
+export async function getStreamToken(req, res) {
+  try {
+    const token = generateStreamToken(req.user.id);
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.log("Error in getStreamToken controller:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
